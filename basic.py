0f568b4afcef66686da88ef05f84c1599cc1a517
Nels Nelson <nels.n.nelson@gmail.com>
2014-05-01 20:45:58 +0000

    Trying to start up and destroy container
    More API probing

--- basic.py
+++ basic.py
@@ -1,24 +1,73 @@
+#! /usr/bin/env python
+
 import libvirt
 import sys
+import os
 
-class Libvirt:
-    def __init__(self):
-        # Either works:
-        #conn = libvirt.openReadOnly('lxc:///')
-        self.conn = libvirt.openReadOnly(None)
+CONFIG = '''<domain type='lxc'>
+  <name>{}</name>
+  <memory>500000</memory>
+  <os>
+    <type>exe</type>
+    <init>/bin/bash</init>
+  </os>
+  <vcpu>1</vcpu>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>destroy</on_crash>
+  <devices>
+    <emulator>/usr/lib/libvirt_lxc</emulator>
+    <console type='pty' />
+  </devices>
+</domain>'''
 
+class Container:
+    def __init__(self, name='test'):
+        self.name = name
+        self.conn = libvirt.open('lxc:///')
         if self.conn == None:
             print 'Failed to open connection to the hypervisor'
 
-    def container(self, name):
+    def create(self, name=None):
+        if not name:
+            name = self.name
         try:
-            return self.conn.lookupByName(name)
+            self.domain = self.conn.lookupByName(name)
         except Exception as ex:
-            print 'Failed to find the domain for {}: {}'.format(name, ex)
+            try:
+                self.domain = self.conn.defineXML(CONFIG.format(name))
+                if self.domain:
+                    result = self.domain.createWithFiles()
+                    print "Created domain: {}".format(result)
+            except Exception as ex:
+                print 'Exception creating domain {}: {}'.format(name, ex)
+
+        print "  Domain is active: {}".format(self.domain.isActive())
+        print "  Domain state: {}".format(self.domain.state())
+
+        return self.domain
+
+    def destroy(self, domain=None, name=None):
+        if not name:
+            name = self.name
+        if not domain:
+            domain = self.domain
+        try:
+            return self.domain.destroy()
+        except Exception as ex:
+            print 'Exception destroying domain {}: {}'.format(name, ex)
+
+    def __enter__(self):
+        self.create()
+        return self.domain
+
+    def __exit__(self, type, value, tb):
+        self.destroy() 
 
 if __name__ == '__main__':
-    server = Libvirt()
-    test = server.container('test')
-    print "Test: id {} running {}".format(test.ID(), test.OSType())
-    print test.info()
+    name = sys.argv[1] if len(sys.argv) > 1 else 'test'
+    with Container(name) as c:
+        print "Test: id {} running {}".format(c.ID(), c.OSType())
+        print c.info()
 
---
+++ libvirtapi.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import libvirt
+from libvirt import virDomain
+import inspect
+import sys
+
+functions = dir(libvirt)
+
+#print "Module libvirt methods:"
+#print functions
+
+functions = dir(virDomain)
+
+#print "Module domain methods:"
+#print functions
+
+#conn = libvirt.openReadOnly(None)
+
+#functions = dir(conn)
+
+#print globals()
+
+
+
+method = None
+
+try:
+    method = getattr(globals()['virDomain'], sys.argv[1])
+except:
+    try:
+        method = getattr(globals()['libvirt'], sys.argv[1])
+    except:
+        pass
+
+if method:
+    help(method)
+
+conn = libvirt.open('lxc:///')
+
+#for f in functions:
+#    print f.func_defaults
+
+print "Connection methods:"
+for m in inspect.getmembers(conn, predicate=inspect.ismethod):
+    print "  {}".format(m)
+
+
